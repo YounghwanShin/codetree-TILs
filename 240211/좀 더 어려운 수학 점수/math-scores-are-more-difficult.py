d289edcccba33a9b6fe6b65a66fb61234cf1be88
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-11 11:12:39 +0900

    Title: 좀 더 어려운 수학 점수, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240211/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,12 +1,7 @@
 AM, AE = map(int, input().split())
 BM, BE = map(int, input().split())
 
-if AM>BM:
-    print("A")
-elif AM<BM:
-    print("B")
+if AM != BM:
+    print("A" if AM > BM else "B")
 else:
-    if AE>BE:
-        print("A")
-    else:
-        print("B")
+    print("A" if AE > BE else "B")
