4fb7dcbec93ada909c5d55cf90c8a4357905c485
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-12 18:23:06 +0900

    Title: a/b 출력, Time: 346ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240212/a/b 출력/a-divide-b.py
+++ 240212/a/b 출력/a-divide-b.py
@@ -1,5 +1,13 @@
-a,b=map(float, input().split())
+from decimal import Decimal, getcontext
 
-c=a/b
+# 소수점 이하 21자리까지 정확도를 보장하도록 설정
+getcontext().prec = 21
 
-print(f"{c:.20f}")
+# 입력 받기
+a, b = map(Decimal, input().split())
+
+# 나눗셈 수행
+result = a / b
+
+# 결과 출력 (소수점 이하 20자리까지)
+print("{:.20f}".format(result))
