2c456e4597a9a9edeec322786c02b871bc1955a4
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-13 16:54:02 +0900

    Title: 100점 도달하기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/100점 도달하기/reach-100-point.py
+++ 240213/100점 도달하기/reach-100-point.py
@@ -1,16 +1,16 @@
 n = int(input())
 
-def prinf(par):
+def printf(par):
     print(par, end=" ")
 
 def grade(score):
     if score>=90:
         printf('A')
-    if score>=80:
+    elif score>=80:
         printf('B')
-    if score>=70:
+    elif score>=70:
         printf('c')
-    if score>=60:
+    elif score>=60:
         printf('D')
     else:
         printf('F')
