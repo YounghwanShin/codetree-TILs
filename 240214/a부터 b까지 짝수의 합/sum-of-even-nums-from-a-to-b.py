61856e37398c5b0f162e3781cec4423b11fde6de
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-14 21:27:51 +0900

    Title: a부터 b까지 짝수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240214/a부터 b까지 짝수의 합/sum-of-even-nums-from-a-to-b.py
@@ -0,0 +1,16 @@
+a,b=map(int,input().split())
+
+def calculate(a,b):
+    val_sum=0
+    for i in range(a,b+1):
+        if i%2==0:
+        val_sum+=i
+    return val_sum
+
+if a<b:
+    print(calculate(a,b))
+elif a>b:
+    print(calculate(b,a))
+else:
+    if a%2==0:
+    print(a)
