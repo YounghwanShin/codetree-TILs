d389c403df4b59d58e749165d0118ff8d9c92ad6
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-21 23:18:53 +0900

    Title: 별표 출력하기 11, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/별표 출력하기 11/print-star-11.py
+++ 240221/별표 출력하기 11/print-star-11.py
@@ -1,14 +1,14 @@
-n=int(input())+3
+n=2*int(input())+1
 
-for i in range(1,n+2):
+for i in range(1,n+1):
     if i%2==0:
-        for k in range(1,n+2):
+        for k in range(1,n+1):
             if k%2==0:
                 print(' ', end=' ')
             else:
                 print('*', end=' ')
         print()
     else:
-        for _ in range(n+1):
+        for _ in range(n):
             print('*', end=' ')
         print()
