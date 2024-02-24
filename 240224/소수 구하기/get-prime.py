86cab5f180fcebbbd379a1484a6e35fc8eec8f18
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-24 22:49:19 +0900

    Title: 소수 구하기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/소수 구하기/get-prime.py
+++ 240224/소수 구하기/get-prime.py
@@ -8,6 +8,6 @@ for i in range(2,n+1):
     prime = True
     for j in range(2, math.isqrt(i) + 1):
         if i % j == 0:
-            print=False
+            prime=False
     if prime == True:
         print(i, end=' ')
