d701ec1b3516d372db6e31a2038e9a3bcd49830f
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-25 22:49:20 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240225/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+global(num)=0
+
+def calculate(grade_arr):
+    grade_standard=[(60,'pass'),(0,'fail')]
+
+    for grade,message in grade_standard:
+        if sum(grade_arr)/len(grade_arr)>=grade:
+            num+=1
+            return message
+
+
+for _ in range(n):
+    grade_arr=list(map(int, input().split()))
+    print(calculate(grade_arr))
+
+print(num)
