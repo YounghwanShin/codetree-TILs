51264a854fb828a21e2fb2d15e61cf94ea6338c9
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-25 22:39:24 +0900

    Title: 배열 채우기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/배열 채우기/filling-array.py
+++ 240225/배열 채우기/filling-array.py
@@ -1,9 +1,7 @@
 arr = list(map(int, input().split()))
 
-try:
-    zero_index = arr.index(0)
-except ValueError:
-    zero_index = len(arr)
+zero_index = arr.index(0) if 0 in arr else len(arr)
+
 
 for num in arr[zero_index-1::-1]:
     print(num, end=' ')
