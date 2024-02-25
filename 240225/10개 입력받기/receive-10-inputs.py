628035bf4be8e738fdcbdeeb8ef3b9f0c357f6f7
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-25 22:39:07 +0900

    Title: 10개 입력받기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/10개 입력받기/receive-10-inputs.py
+++ 240225/10개 입력받기/receive-10-inputs.py
@@ -1,9 +1,6 @@
 arr = list(map(int, input().split()))
 
-try:
-    zero_index = arr.index(0)
-except ValueError:
-    zero_index = len(arr)
+zero_index = arr.index(0) if 0 in arr else len(arr)
 
 sum_arr=sum(arr[:zero_index])
 ave_arr=sum_arr/zero_index
