b7e4ad2ad0727beb7e7645a8b8cc881b1128d590
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-26 16:56:15 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240226/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,7 +1,10 @@
-arr = list(map(int, input().split()))
+arr=list(map(int, input().split()))
 
-first_zero_index = next((i for i, k in enumerate(arr) if k == 0), -1)
+zero_index = [i for i, k in enumerate(arr) if k == 0]
 
-cnt = sum(arr[first_zero_index-3:first_zero_index]) if first_zero_index >= 3 else 0
+cnt=0
+
+for i in range(zero_index[0]-3,zero_index[0]):
+    cnt+=arr[i]
 
 print(cnt)
