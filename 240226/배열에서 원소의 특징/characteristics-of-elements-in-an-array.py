a6076eebac2443208e28940d5d777646caf6f06c
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-26 18:15:32 +0900

    Title: 배열에서 원소의 특징, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 240226/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -2,4 +2,4 @@ arr = list(map(int, input().split()))
 
 multiples_of_3_index = next((i for i, num in enumerate(arr) if num % 3 == 0))
 
-print(arr[multiples_of_3_index[0] - 1])
+print(arr[multiples_of_3_index - 1])
