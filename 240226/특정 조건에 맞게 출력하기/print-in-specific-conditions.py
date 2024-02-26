bdbb5233195a57395f75bc15d63e1e8d9e529251
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-26 21:16:15 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
+++ 240226/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -3,8 +3,10 @@ arr = list(map(int, input().split()))
 i=0
 
 while i<=len(arr)-1:
-    if arr[i]%2==0 and arr[i]!=0:
+    if arr[i]==0:
+        break
+    if arr[i]%2==0:
         print(arr[i]//2, end=' ')
-    elif arr[i]!=0:
+    else:
         print(arr[i]+3, end=" ")
     i+=1
