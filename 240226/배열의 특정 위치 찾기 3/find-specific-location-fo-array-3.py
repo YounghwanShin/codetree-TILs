62b3c0ab3521aed23e28d5e93e666be54df71dc2
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-26 16:37:44 +0900

    Title: 배열의 특정 위치 찾기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240226/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240226/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,10 +1,10 @@
-arr = list(map(int, input().split()))
+arr=list(map(int, input().split()))
 
-cnt = 0
+zero_index = [i for i, k in enumerate(arr) if k == 0]
 
-for i, num in enumerate(arr):
-    if num == 0:
-        break
-    cnt += num
+cnt=0
+
+for i in range(zero_index[0]-3:zero_index[0]):
+    cnt+=arr[i]
 
 print(cnt)
