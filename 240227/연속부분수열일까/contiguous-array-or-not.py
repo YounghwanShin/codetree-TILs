9ed6f437217d52774b446e1f4913d65b8128a365
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-27 17:00:42 +0900

    Title: 연속부분수열일까, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/연속부분수열일까/contiguous-array-or-not.py
+++ 240227/연속부분수열일까/contiguous-array-or-not.py
@@ -5,14 +5,12 @@ arr_B = list(map(int, input().split()))
 
 start_indices = [i for i, x in enumerate(arr_A) if x == arr_B[0]]
 
-for start in start_indices:
-    if arr_A[start:start+len(arr_B)] == arr_B:
-        print('Yes')
-        break
-    else:
-        if start==start_indices[-1]:
-            print('No')
-        continue
-
-if len(start_indices)==0:
+if len(start_indices) == 0:
     print('No')
+else:
+    for start in start_indices:
+        if arr_A[start:start+len(arr_B)] == arr_B:
+            print('Yes')
+            break
+    else:
+        print('No')
