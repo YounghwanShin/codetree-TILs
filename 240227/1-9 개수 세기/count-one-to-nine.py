52460bdc56b91641950f3e380a0d1a1448477bca
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-27 14:30:03 +0900

    Title: 1-9 개수 세기, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/1-9 개수 세기/count-one-to-nine.py
+++ 240227/1-9 개수 세기/count-one-to-nine.py
@@ -2,12 +2,11 @@ n=int(input())
 
 arr = list(map(int, input().split()))
 
-max_val = max(arr)
-count_arr = [0] * (max_val+1)
+count_arr = [0] *9
 
 for elem in arr:
     count_arr[elem-1] += 1
 
-for i in range(max_val + 1):
+for i in range(9):
     cnt = count_arr[i]
     print(f"{cnt}")
