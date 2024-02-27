cf47353cc33560b832ab09879afbc68d3639c0f5
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-27 16:21:35 +0900

    Title: 배열 놀이, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/배열 놀이/play-with-array.py
+++ 240227/배열 놀이/play-with-array.py
@@ -1,21 +1,21 @@
-n,q=map(int,input().split())
-
-arr=list(map(int, input().split()))
+n, q = map(int, input().split())
+arr = list(map(int, input().split()))
 
 for _ in range(q):
-    arr_question=list(map(int, input().split()))
-    if arr_question[0]==1:
-        a=arr_question[1]
+    query = list(map(int, input().split()))
+    query_type = query[0]
+    
+    if query_type == 1:
+        a = query[1]
         print(arr[a-1])
-    elif arr_question[0]==2:
-        a=arr_question[1]
-        if a in arr:
-            print(arr.index(a)+1)
-        else:
+    elif query_type == 2:
+        a = query[1]
+        try:
+            print(arr.index(a) + 1)
+        except ValueError:
             print(0)
     else:
-        a,b=arr_question[1],arr_question[2]
-        for i in range(a-1,b):
-            print(arr[i],end=' ')
+        a, b = query[1], query[2]
+        for i in range(a-1, b):
+            print(arr[i], end=' ')
         print()
-    arr_question=[]
