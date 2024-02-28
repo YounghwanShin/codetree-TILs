9cf456b6053bbb04a4c468b7d7fc607c5d8ac257
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-28 10:27:53 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240228/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240228/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -2,16 +2,22 @@ n=int(input())
 
 arr=list(map(int, input().split()))
 
-for i in range(n):
-    max_index=i
-    for j in range(i+1,n):
-        if arr[max_index]<arr[j]:
-            max_index=j
-    arr[i],arr[max_index]=arr[max_index],arr[i]
-
-if arr[0]!=arr[1]:
+if n==1:
     print(arr[0])
 else:
-    for k in range(1,n):
-        if arr[k]!=arr[k+1] and arr[k-1]!=arr[k]:
-            print(arr[k])
+    for i in range(n):
+        max_index=i
+        for j in range(i+1,n):
+            if arr[max_index]<arr[j]:
+                max_index=j
+        arr[i],arr[max_index]=arr[max_index],arr[i]
+
+    if arr[0]!=arr[1]:
+        print(arr[0])
+    else:
+        for k in range(1,n):
+            if arr[k]!=arr[k+1] and arr[k-1]!=arr[k]:
+                print(arr[k])
+                break
+        else:
+            print(-1)
