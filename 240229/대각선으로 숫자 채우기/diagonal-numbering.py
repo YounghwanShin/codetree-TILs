db0f554d5e9aac316b613bc7f8d30ccb39ad2690
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-02-29 14:44:02 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240229/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240229/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -4,20 +4,29 @@ arr=[[0 for _ in range(m)] for _ in range(n)]
 
 cnt=1
 
-for j in range(m):
-    i=0
-    while j>=0:
-        arr[i][j]=cnt
+if n==1:
+    for j in range(m):
+        arr[0][j]=cnt
         cnt+=1
-        i,j=i+1,j-1
+elif m==1:
+    for i in range(n):
+        arr[i][n]=cnt
+        cnt+=1
 else:
-    if n>=1:
-        for i in range(1,n):
-            j=m-1
-            while i<n:
-                arr[i][j]=cnt
-                cnt+=1
-                i,j=i+1,j-1
+    for j in range(m):
+        i=0
+        while j>=0:
+            arr[i][j]=cnt
+            cnt+=1
+            i,j=i+1,j-1
+    else:
+        if m>1:
+            for i in range(1,n):
+                j=m-1
+                while i<n:
+                    arr[i][j]=cnt
+                    cnt+=1
+                    i,j=i+1,j-1
 
 for i in range(n):
     for j in range(m):
