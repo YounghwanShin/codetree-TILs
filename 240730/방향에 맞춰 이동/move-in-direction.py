d19eec934159848421f463f3e011f1700fd4aa01
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-07-30 19:23:53 +0900

    Title: 방향에 맞춰 이동, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/방향에 맞춰 이동/move-in-direction.py
+++ 240730/방향에 맞춰 이동/move-in-direction.py
@@ -1,13 +1,14 @@
-N=int(input())
-x, y = 0,0
+N = int(input())
+x, y = 0, 0
 
 dx, dy = [-1, 0, 0, 1], [0, -1, 1, 0]
-
-dic={'W':0,'S':1,'N':2,'E':3}
+dic = {'W': 0, 'S': 1, 'N': 2, 'E': 3}
 
 for _ in range(N):
-    a,b=input().split()
-    b=int(b)
-    x,y=x+dx[dic[a]]*b, y+dy[dic[a]]*b
+    a, b = input().split()
+    b = int(b)
+    dir_inx = dic[a]
+    x += dx[dir_inx] * b
+    y += dy[dir_inx] * b
 
-print(x,y)
+print(x, y)
