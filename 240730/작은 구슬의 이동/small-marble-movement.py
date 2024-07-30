8f7212bc6163d5344d689b8a51edc8f355eb4eff
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-07-30 22:11:40 +0900

    Title: 작은 구슬의 이동, Time: 223ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/작은 구슬의 이동/small-marble-movement.py
+++ 240730/작은 구슬의 이동/small-marble-movement.py
@@ -1,41 +1,27 @@
-"""
-        Y                (x, y + 1)
-        ^                    ^
-        |                    | 3
-        |              2     |      0
-        | (x - 1, y) <------ (x, y) -----> (x + 1, y)
-        |                    | 1
-        |                    v
-        |                (x, y - 1)
-        |
----------------------------------------------> X
-"""
-
 n, t = map(int, input().split())
-dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
 mapper = {
-    'R': 0,  
-    'D': 1,  
-    'L': 2,  
-    'U': 3   
+    'R': 0,
+    'D': 1,
+    'L': 2,
+    'U': 3
 }
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x < n and 0 <= y < n
 
-arr = input().split()
-x, y = int(arr[1]) - 1, int(arr[0]) - 1 
-c_dir = arr[2]
+r, c, d = input().split()
+x, y = int(r) - 1, int(c) - 1
 
-move_dir = mapper[c_dir]
+move_dir = mapper[d]
 
 for _ in range(t):
     nx, ny = x + dxs[move_dir], y + dys[move_dir]
     if not in_range(nx, ny):
         move_dir = (move_dir + 2) % 4
-        continue 
+        continue
     
     x, y = nx, ny
 
-print(y + 1, x + 1)
+print(x + 1, y + 1)
