87c4a02a20990e667c5f2fa1fadd51fd13751744
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-07-31 21:27:47 +0900

    Title: 되돌아오기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/되돌아오기/come-back.py
@@ -0,0 +1,22 @@
+N=int(input())
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+dir_map = {"E":0, "S":1, "W":2, "N":3}
+x, y = 0, 0
+time = 0
+val=False
+
+for _ in range(N):
+    temp = input().split()
+    dir_num = dir_map[temp[0]]
+    size = int(temp[1])
+
+    for _ in range(1,size+1):
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+        time += 1
+        if x==0 and y==0:
+            print(time)
+
+            break
+
+if val==False:
+    print(-1)
