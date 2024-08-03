bb8eb7e02fda9f6c6acde4da7cee96b290fdbabc
하리보 <65943655+YounghwanShin@users.noreply.github.com>
2024-08-03 23:19:37 +0900

    Title: 최고의 33위치, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/최고의 33위치/best-place-of-33.py
+++ 240803/최고의 33위치/best-place-of-33.py
@@ -1,26 +1,14 @@
-N=int(input())
-
+N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 def count_coin(row_s, row_e, col_s, col_e):
-    val=0
+    return sum(arr[r][c] for r in range(row_s, row_e + 1) for c in range(col_s, col_e + 1))
 
-    for r in range(row_s,row_e+1):
-        for c in range(col_s,col_e+1):
-            val+=arr[r][c]
+max_val = 0
 
-    return val
-
-max_val=0
-
-for i in range(N):
-    for k in range(N):
-
-        if i+2>=N or k+2>=N:
-            continue
-        
-        current_val=count_coin(i,i+2,k,k+2)
-
-        max_val=max(max_val,current_val)
+for i in range(N - 2): 
+    for k in range(N - 2):  
+        current_val = count_coin(i, i + 2, k, k + 2)  
+        max_val = max(max_val, current_val)
 
 print(max_val)
